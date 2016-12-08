4f20352d6df9d4d5b1e5ddb979766836b8f47dc0
packocrayons <gibson.brydon@gmail.com>
2016-12-08 14:16:44 -0500

    everything working - no db editor as of yet, but that's an easy add

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,11 +18,17 @@ using namespace std;
 #define DB_MSG_NAME "/db_msg"
 
 #define DATABASE_FILENAME "../database.db"
+#define TEMPFILE_FILENAME "../tempFile.db"
 
 typedef struct Pin_msg_struct{
 	char acctNum[6];
 	char PIN[4];
 } Pin_msg_struct;
+
+typedef struct Fund_request_struct{
+	char requestType[2]; //FR for request funds, WF for withdraw, RR returned request, RW returned withdraw
+	char fundRequest[8];
+} Fund_request_struct;
 
 typedef struct DB_msg_struct{
 	char message[6]; //only thing for now
@@ -41,6 +48,7 @@ static mqd_t PIN_MSG = -1;
 static mqd_t DB_MSG = -1;
 
 pthread_mutex_t printing_lock;
+pthread_mutex_t file_working_lock;
 
 /*****************UTILITIES*************/
 void lockAndPrint(string printThis){
@@ -70,6 +78,16 @@ char* decryptPIN(char* encryptedPIN){
 	return decryptedPin;
 }
 
+char* intToCharArray(int num){
+	int n = log10(num);
+	char* arr = (char*) calloc(n + 1, sizeof(char));
+	for (int i = n; i >= 0; --i, num /= 10)
+	{
+		arr[i] = (num % 10) + '0';
+	}
+	return arr;
+}
+
 /******************MISC******************/
 void sig_handler(int signum){
 	//ASSERT(signum == SIGINT);
@@ -91,6 +109,7 @@ void sig_handler(int signum){
 
 int main(int argc, char const *argv[])
 {
+	cout.precision(9);	
 	pthread_attr_t attr;
 
 	signal(SIGINT, sig_handler);
@@ -98,6 +117,10 @@ int main(int argc, char const *argv[])
 
 	/*************************MUTEX INIT***************/
 	int rc = pthread_mutex_init(&printing_lock, NULL);
+	if (rc < 0){
+		perror("Error creating mutex");
+	}
+	rc = pthread_mutex_init(&file_working_lock, NULL);
 	if (rc < 0){
 		perror("Error creating mutex");
 	}
@@ -136,6 +159,7 @@ int main(int argc, char const *argv[])
 
 	/******************MUTEX DESTROY******************/
 	pthread_mutex_destroy(&printing_lock);
+	pthread_mutex_destroy(&file_working_lock);
 
 	sig_handler(SIGINT);
 }
@@ -176,9 +200,39 @@ void* run_ATM(void* arg) {
 			perror("receiving DB message failed");
 			exit(0);
 		}
-		if(arrayEquals(rcv_message.message, "OKAY", 4)) cout << "OKAY" << endl;
+		if(arrayEquals(rcv_message.message, "OKAY", 4)){
+			
+			char inputVal[20];
+			char fundRequest[10];
+			Fund_request_struct frs;			
+
+			lockAndPrint("Login successful, requestFunds or withdraw-(quantity) > ");
 
 
+			while(1){
+				cin >> inputVal;
+				if (arrayEquals(inputVal, "requestFunds", 13)){
+					memcpy(frs.requestType, "FR", 2);					
+					mq_send(PIN_MSG, (const char*) &frs, sizeof(frs), 1);
+					mq_receive(PIN_MSG, (char *) &frs, sizeof(frs), NULL);
+					pthread_mutex_lock(&printing_lock);
+					cout << "Fund request made, funds available : " << frs.fundRequest << endl;
+					pthread_mutex_unlock(&printing_lock);
+					break;
+				}
+				if (arrayEquals(inputVal, "withdraw", 8)) {
+					memcpy(frs.requestType, "WF", 2);
+					memcpy(frs.fundRequest, &inputVal[9], 11);
+					mq_send(PIN_MSG, (char*) &frs, sizeof(frs), 1);
+					mq_receive(PIN_MSG, (char*) &frs, sizeof(frs), NULL);
+					cout << "Withdrawal result: " << frs.fundRequest << endl;
+					break;
+				} 				
+				lockAndPrint("Invalid input, please try again > ");
+			}
+
+
+		} else lockAndPrint("Username or password was incorrect\n");
 	}
 }
 
@@ -188,18 +242,16 @@ void* run_DB(void* arg){
 	char working_acct_number[5];
 	int working_acct_number_attempts = 0;
 
+	// bool dirty_file = 0; //file is modified and has to be written out to file, then this bit can be cleared. If dirty_file is set, we should have file_working_lock.
+
 	char line[20];
 
 	lockAndPrint("Database server running\n");
 	int status;
 	Pin_msg_struct rcv_message;
-	DB_msg_struct message;
+	DB_msg_struct db_message;
+	Fund_request_struct fr_message;
 
-	database = fopen(DATABASE_FILENAME, "rw");
-	if(database == NULL){
-		lockAndPrint("ERROR - FILE NOT FOUND");
-		exit(0);
-	}
 
 	while(1){
 		status = mq_receive(PIN_MSG, (char*) &rcv_message, sizeof(rcv_message), NULL); //we have the PIN and acctNum here, for safety, only use the first 5 and 3 chars respectively
@@ -209,23 +261,118 @@ void* run_DB(void* arg){
 		}
 		lockAndPrint("Message received - checking for valid input\n");
 		char* validReturn;
+		pthread_mutex_lock(&file_working_lock);
+		database = fopen(DATABASE_FILENAME, "r+");
+		if(database == NULL){
+			lockAndPrint("ERROR - FILE NOT FOUND");
+			exit(0);
+		}
+
 		do{
 			validReturn = fgets(line, 20, database);
 			if (validReturn == NULL) break;
 		} while(!arrayEquals(line, rcv_message.acctNum, 5)); //if the first 5 chars of the line match the 5 chars in accountNum)
+
 		//if we get here, either we broke from the loop due to EOF, or we matched successfully
+		fclose(database);
+		pthread_mutex_unlock(&file_working_lock);
 		if(validReturn == NULL) {
-			memcpy(message.message, "NOT OK", strlen("NOT OK") + 1); //send a NOT OKAY message
+			memcpy(db_message.message, "NOT OK", strlen("NOT OK") + 1); //send a NOT OKAY message
 		} else {
+			
+			if (!arrayEquals(working_acct_number, rcv_message.acctNum, 5)) memcpy(working_acct_number, rcv_message.acctNum, 5); //update the working account number
 			char decryptedPin[3];
 			memcpy(decryptedPin, decryptPIN(rcv_message.PIN), 3 + 1);
 			if (arrayEquals(&line[6], decryptedPin, 3)){
-				memcpy(message.message, "OKAY", strlen("OKAY") + 1); //send an OKAY message
-			} else cout << "pin was not valid "; //add counter here
+				memcpy(db_message.message, "OKAY", strlen("OKAY") + 1); //send an OKAY message
+			} else {
+				memcpy(db_message.message, "NOT OK", strlen("NOT OK") + 1); //send a NOT OKAY message
+				working_acct_number_attempts++;
+			}
+
 		}
 
-		status = mq_send(DB_MSG, (const char*) &message, sizeof(message), 1);
+		if (working_acct_number_attempts > 3){ //lock the account
+			pthread_mutex_lock(&file_working_lock); //the file is not dirty, we don't have the lock. If we want to write to the file, we need the lock
+				database = fopen(DATABASE_FILENAME, "r+");
+				if(database == NULL){
+					lockAndPrint("ERROR - FILE NOT FOUND");
+					exit(0);
+				}
+				fseek(database, -strlen(line), SEEK_CUR);
+				fprintf(database, "X");
+				fclose(database);
+			pthread_mutex_unlock(&file_working_lock);
+
+			lockAndPrint("Too many bad password attempts - account locked\n");
+		}
+
+		status = mq_send(DB_MSG, (const char*) &db_message, sizeof(db_message), 1);
 		if (status < 0) perror("error sending db message");
 
+		status = mq_receive(PIN_MSG, (char*) &fr_message, sizeof(fr_message), NULL);
+		if (status < 0) perror("error receiving fundRequest message");
+
+		if (arrayEquals(fr_message.requestType, "FR", 2)){ //it's a fund request, send back the funds in this account
+			memcpy(fr_message.requestType, "RR", 2);
+			for (int i = 0; (i < 8) && (line[10+i] != '\n'); ++i)
+			{
+				fr_message.fundRequest[i] = line[10+i];
+			}
+		} else if (arrayEquals(fr_message.requestType, "WF", 2)){ //it's a withdraw funds request - try to withdraw the funds and send back "ENOUGH" or "LOW FUNDS"
+			lockAndPrint("Withdrawing funds\n");
+			pthread_mutex_lock(&file_working_lock); //lock the file
+				database = fopen(DATABASE_FILENAME, "r+");
+				if(database == NULL){
+					lockAndPrint("ERROR - FILE NOT FOUND");
+					exit(0);
+				}
+
+				FILE* newFile;
+				char* vr;
+				char mLine[20];
+				
+				rewind(database);
+				newFile = fopen(TEMPFILE_FILENAME, "w+");
+				if(newFile == NULL){
+					lockAndPrint("ERROR OPENING TEMPORARY FILE");
+					exit(0);
+				}			
+
+				vr = fgets(mLine, 20, database);
+
+				while(1){ //exit this loop when it's broken by vr==NULL
+					while(!arrayEquals(mLine, rcv_message.acctNum, 5)) { //if the first 5 chars of the line match the 5 chars in accountNum)
+						// cout << mLine << endl; //DEBU
+						fprintf(newFile, "%s", mLine);
+						if (vr == NULL) break;
+						vr = fgets(mLine, 20, database);
+					} 
+					if (vr == NULL) break; //we hit EOF, we're done and we can leave the outer loop
+					//if we get here, we didn't hit the end of the file, that means we're at the line we want to modify.
+					float money = atof(&line[10]);
+					float withdrawal = atof(fr_message.fundRequest);
+					if (withdrawal <= money) { //if there's enough money
+						money -= withdrawal;
+						for (int i = 0; i < 10; ++i)
+						{	
+							fprintf(newFile, "%c", mLine[i]); //the first half of this line is unchanged
+						}
+						fprintf(newFile, "%.2f\n", money); //print out the new money
+						memcpy(fr_message.fundRequest, "ENOUGH", strlen("ENOUGH") + 1); //this is what we're going to send
+					} else memcpy(fr_message.fundRequest, "LO FUNDS", strlen("LO FUNDS") + 1); //don't +1 because there's no room in the array.
+					vr = fgets(mLine, 20, database); //fetch the next line for the processing loop
+				}
+				memcpy(fr_message.requestType, "RW", 2);
+				fflush(newFile);
+				remove(DATABASE_FILENAME);
+				rename(TEMPFILE_FILENAME, DATABASE_FILENAME);
+
+				fclose(database);
+			pthread_mutex_unlock(&file_working_lock); //we're done with the file now
+		}
+		mq_send(PIN_MSG, (const char*) &fr_message, sizeof(fr_message), 1);
+
 	}
+	fclose(database);
 }
